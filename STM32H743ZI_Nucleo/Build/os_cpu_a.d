efa2eb3b83db2d992ebf0f7ce95112b4b905152a
srajandekar <s.rajandekar@amazilia.aero>
2024-01-27 20:14:54 +0100

    Deleted files

--- STM32H743ZI_Nucleo/Build/os_cpu_a.d
+++
@@ -1,2 +0,0 @@
-/home/surabhi/Projects/micrium_stm32h743zi-nucleo_blinky/STM32H743ZI_Nucleo/Build/os_cpu_a.o: \
- /home/surabhi/Projects/micrium_stm32h743zi-nucleo_blinky/STM32H743ZI_Nucleo/Micrium/Software/uCOS-III/Ports/ARM-Cortex-M/ARMv7-M/GNU/os_cpu_a.s
